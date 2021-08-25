e30fe3abccd63fa42272f87cb933c17d9ebc5649
luckman212 <luke.hamburg@gmail.com>
2021-08-25 10:07:44 -0400

    better parsing and handling of edge cases
    - use plugin id instead of repo name to avoid collisions e.g. `obsidian-find-and-replace-in-selection`
    - workaround for plugins with pipe `|` in name e.g. larslockefeer/obsidian-plugin-todo

--- obsidian-plugin-downloader.sh
+++ obsidian-plugin-downloader.sh
@@ -58,7 +58,7 @@ _mkdir
 if command -v gh &>/dev/null; then
   use_graphql=true
   function _download {
-    if gh repo clone "${1#$pfx/}" ${2:+-- --branch "$2"} &>/dev/null; then (( c++ )); fi;
+    if gh repo clone "${1#$pfx/}" "$2" ${3:+-- --branch "$3"} &>/dev/null; then (( c++ )); fi;
   }
 else
   if ! command -v git &>/dev/null; then
@@ -67,30 +67,31 @@ else
   fi
   echo 1>&2 "% gh not present, falling back to git"
   function _download {
-    if git clone "$1" &>/dev/null; then (( c++ )); fi;
+    if git clone ${3:+--branch "$3"} -- "$1" "$2" &>/dev/null; then (( c++ )); fi;
   }
 fi
 
-while IFS='|' read -r URL _ ; do
+while IFS=$'\t' read -r URL _ ID ; do
   GH_USER_REPO=${URL#$pfx/}
-  REPO_ONLY=${GH_USER_REPO##*/}
   if [[ $use_graphql == true ]]; then
     unset CUR_VER LATEST_VER
-    CUR_VER=$(jq -r '.version' "$REPO_ONLY/manifest.json" 2>/dev/null)
+    CUR_VER=$(jq -r '.version' "$ID/manifest.json" 2>/dev/null)
     LATEST_VER="$(_fetchVersion "$GH_USER_REPO")"
     if [[ "$LATEST_VER" == "$CUR_VER" ]]; then
       echo "$GH_USER_REPO: v${CUR_VER} already downloaded ✓"
       continue
     else
-      rm -rf "${REPO_ONLY:?}"
+      rm -rf "${ID:?}"
     fi
   fi
   echo "$GH_USER_REPO: downloading${CUR_VER:+ v$CUR_VER →}${LATEST_VER:+ v$LATEST_VER}"
-  _download "$URL" "$LATEST_VER"
+  if [[ -n "$ID" ]]; then
+    _download "$URL" "$ID" "$LATEST_VER"
+  fi
 done < <(
-  jq -r 'sort_by(.name) | .[] | [ .repo, .name, .description ] | @tsv' <<<"$JSON" |
+  jq -r 'sort_by(.name) | .[] | [ .repo, .name, .description, .id ] | @tsv' <<<"$JSON" |
   tr -cd '[[:print:]]\n\t' |
-  awk -v pfx=$pfx 'BEGIN { FS="\t" } { printf "%s/%s\t%s: %s\n",pfx,$1,$2,$3 }' |
+  awk -v pfx=$pfx 'BEGIN { FS="\t" } { printf "%s/%s\t%s: %s\t%s\n",pfx,$1,$2,$3,$4 }' |
   sed $'s/\t/\u00a0\t/g' |
   column -s$'\t' -t |
   fzf -i ${1:+-q $1} --exact --multi --no-hscroll --no-mouse --no-select-1 \
@@ -100,6 +101,6 @@ done < <(
       --bind='ctrl-a:select-all,ctrl-s:deselect-all' \
       --preview-window='down,3,wrap' \
       --preview='echo {2}' |
-  sed $'s/\u00a0\ */|/g'
+  sed $'s/\u00a0\ */\t/g'
 ) |
 awk -F: '{ printf "%-60s %s\n",$1,$2 }'
